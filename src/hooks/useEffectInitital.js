49db88083d9855aedb6638ec96bc96230ef2f76b
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-22 23:58:51 +0300

    useEffectInitial added

---
+++ src/hooks/useEffectInitital.js
@@ -0,0 +1,16 @@
+import { useRef, useEffect } from "react"
+
+const useEffectInitial = (callback, dependencies)  => {
+    const isInitial = useRef(true);
+
+    useEffect(() => {
+        if(isInitial.current){
+            isInitial.current = false;
+            return;
+        }
+        callback();
+    }, dependencies);
+
+}
+
+export default useEffectInitial
