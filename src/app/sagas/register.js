2208eba45dd44e6bacabc6674028358f719b9ef5
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-02 22:57:16 +0300

    registerWatcher added

---
+++ src/app/sagas/register.js
@@ -0,0 +1,30 @@
+import { BASE_URL } from "../../constants"
+import { put, takeLatest } from "redux-saga/effects";
+import Router from 'next/router';
+import { onRegisterComplete } from "../slicers/authenticate";
+
+export default takeLatest('authenticate/registerRequest', register)
+
+function* register({payload}){
+    const response = yield fetch(`${BASE_URL}/users/register`,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'Application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+
+    const data = yield response.json();
+    if(response.ok){
+        yield put(onRegisterComplete({
+            user: data,
+        }))
+        
+        localStorage.setItem('Authorization', response.headers.get('Authorization'));
+        Router.push('/')
+    }else{
+        yield put(onRegisterComplete({
+            error: data,
+        }))
+    }
+}
