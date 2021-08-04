843f2cbf02235d7bc1a2a78247063fb7042e0773
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-04 23:34:21 +0300

    createProperty watcher added

---
+++ src/app/sagas/createProperty.js
@@ -0,0 +1,28 @@
+import { BASE_URL } from "../../constants";
+import Router from 'next/router';
+import { onCreatePropertyComplete } from "../slices/createPropertySlice";
+
+const { takeLatest, put } = require("redux-saga/effects");
+
+export default takeLatest('createProperty/createPropertyRequest', createProperty);
+
+function* createProperty({payload}){
+    const response = yield fetch(`${BASE_URL}/properties/auth/create`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'Application/json',
+            'Authorization': `Bearer ${localStorage.getItem('Authorization')}`
+        },
+        body: JSON.stringify(payload)
+    })
+
+    const data = yield response.json();
+    if(!response.ok){
+        console.log('here')
+        yield put(onCreatePropertyComplete({
+            error: data
+        }))
+    }else{
+        Router.push('/');
+    }
+}
