7c97768917e0441316252a33aa0df7e8e15d1c1f
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-04 23:38:54 +0300

    create-property inputs change + dispatch on submit

--- src/pages/create-property.jsx
+++ src/pages/create-property.jsx
@@ -1,22 +1,48 @@
 import useInput from "../hooks/useInput"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import InputWithError from "../components/InputWithError";
+import { getCreatePropertyState, createPropertyRequest } from "../app/slices/createPropertySlice";
 
 const CreateProperty = () => {
     const [createValues, { nameInput, priceInput, locationInput, sizeInput, descriptionInput }] = createInputs(); 
+    const [bedrooms, setBedrooms] = useState(1);
+    const dispatch = useDispatch();
+    const {isLoading, error} = useSelector(getCreatePropertyState);
+
+    const submit = (e) => {
+        e.preventDefault();
+        if(!isLoading){
+            createValues.bedrooms = bedrooms
+            dispatch(createPropertyRequest(createValues))
+        }
+    }
 
     return(
         <section>
             <h1>CreateProperty</h1>
-            <label>Name:</label>
-            {nameInput}
-            <label>Price:</label>
-            {priceInput}
-            <label>Location:</label>
-            {locationInput}
-            <label>Size:</label>
-            {sizeInput}
-            <label>description:</label>
-            {descriptionInput}
+            <form onSubmit={submit}>
+                <label htmlFor='name'>Name:</label>
+                <InputWithError input={nameInput} error={error?.name}/>
+                <label htmlFor='price'>Price:</label>
+                <InputWithError input={priceInput} error={error?.price}/>
+                <label htmlFor='location'>Location:</label>
+                <InputWithError input={locationInput} error={error?.location}/>
+                <label htmlFor='size'>Size:</label>
+                <InputWithError input={sizeInput} error={error?.size}/>
+                <label htmlFor='description'>description:</label>
+                <InputWithError input={descriptionInput} error={error?.description}/>
+                <select value={bedrooms} onChange={(e) => setBedrooms(e.target.value)}>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                </select>
+                <button>submit</button>
+            </form>
         </section>
     )
 }
