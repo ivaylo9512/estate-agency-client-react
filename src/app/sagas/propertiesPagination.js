9268751f1e2ec767760aa10ebede01fb75e2dbbc
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-04 21:00:36 +0300

    split property pagination watcher

--- src/app/sagas/propertiesPagination.js
+++ src/app/sagas/propertiesPagination.js
@@ -1,6 +1,5 @@
 import { BASE_URL } from "../../constants";
 import { setProperties } from "../slicers/propertiesPagination-slicer";
-import Properties from "../../pages/properties";
 
 const { takeEvery, select, put } = require("redux-saga/effects");
 
@@ -9,11 +8,33 @@ export default takeEvery('propertiesPagination/getProperties', getProperties)
 const getPropertiesState = state => state.propertiesPagination.data;
 
 function* getProperties({payload: query}) {
-    const state = yield select(getPropertiesState);
+    const { minPrice, maxPrice, location, bedrooms, take, direction, pages, lastId } =  getQueryData(query, yield select(getPropertiesState))
 
-    let { minPrice, maxPrice, location, bedrooms, take, direction } = query;
+    const response = yield fetch(`${BASE_URL}/properties/findByWithPage/${pages}/${location}/${bedrooms}/${minPrice}/${maxPrice}/${lastId}/${direction}`)
+    const data = yield response.json();
+
+    data.length = data.properties.length;
+    data.properties = splitProperties(data, take)
+
+    yield put(setProperties({
+        data,
+        query
+    }))
+}
+
+const splitProperties = (data, take) => {
+    return data.properties.reduce((result, property, i) => {
+        const page = Math.floor(i  / take);
+        result[page] = result[page] ? (result[page].push(property), result[page]) : [property]
+
+        return result; 
+    }, []);
+}
+
+const getQueryData = (query, state) => {
+    let minPrice = query.minPrice;
     let lastId = 0;
-    let pages = take * query.pages;
+    let pages = query.take * query.pages;
 
     if(minPrice == undefined){
         const lastPage = state.properties[state.properties.length - 1];
@@ -23,20 +44,6 @@ function* getProperties({payload: query}) {
         minPrice = lastProperty.price; 
     }
 
-    const response = yield fetch(`${BASE_URL}/properties/findByWithPage/${pages}/${location}/${bedrooms}/${minPrice}/${maxPrice}/${lastId}/${direction}`)
-    const data = yield response.json();
-
-    data.length = data.properties.length;
-    data.properties = data.properties.reduce((result, property, i) => {
-        const page = Math.floor(i  / take);
-        result[page] = result[page] ? (result[page].push(property), result[page]) : [property]
-
-        return result; 
-    }, []);
-
-    yield put(setProperties({
-        data,
-        query
-    }))
+    return {...query, minPrice, lastId, pages}
 }
 
