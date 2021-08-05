f46d1d223826ac7f2ef9b293ab0b81354ec0b199
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-05 18:04:10 +0300

    UserPropertiesList added

---
+++ src/components/UserPropertiesList.jsx
@@ -0,0 +1,36 @@
+import { useDispatch } from "react-redux"
+import { useSelector } from "react-redux";
+import { getUserPropertiesState, getUserProperties, resetUserPropertiesState, setCurrentUserProperties } from "../app/slices/userPropertiesPaginationSlice";
+import { useEffect } from "react";
+import PropertiesPagination from "./PropertiesPagination";
+import LoadingIndicator from "./LoadingIndicator";
+
+const UserPropertiesList = () => {
+    const dispatch = useDispatch();
+    const {query, data: {currentProperties, isLoading}} = useSelector(getUserPropertiesState);
+
+    useEffect(() => {
+        dispatch(getUserProperties({...query, pages: 1}));
+
+        return () => dispatch(resetUserPropertiesState());
+    }, [])
+
+    return(
+        <div>
+            {currentProperties &&
+                <>
+                    { isLoading 
+                        ? <LoadingIndicator />
+                        : currentProperties.map(property => 
+                            <div key={property.id}>
+                                {property.id}
+                                {property.name}
+                            </div>
+                    )}
+                    <PropertiesPagination selector={getUserPropertiesState} getProperties={getUserProperties} setCurrentProperties={setCurrentUserProperties} />
+                </>
+            }
+        </div>
+    )
+}
+export default UserPropertiesList
