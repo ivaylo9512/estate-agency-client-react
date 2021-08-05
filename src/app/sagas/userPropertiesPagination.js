2ffb29ebb6f23a4fae4e2e73c5d9c45fdf6ffcca
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-05 18:09:00 +0300

    userPropertiesPagination queryData + url change + add authorization header

--- src/app/sagas/userPropertiesPagination.js
+++ src/app/sagas/userPropertiesPagination.js
@@ -1,21 +1,24 @@
 import { BASE_URL } from "../../constants";
-import { setProperties, getUserPropertiesData } from "../slices/userPropertiesPaginationSlice";
+import { setProperties, getUserPropertiesData, setUserProperties } from "../slices/userPropertiesPaginationSlice";
 
 const { takeEvery, select, put } = require("redux-saga/effects");
 
-export default takeEvery('userPropertiesPagination/getProperties', getProperties)
+export default takeEvery('userPropertiesPagination/getUserProperties', getProperties)
 
 function* getProperties({payload: query}) {
-    const { name, take, direction } = query;
-    const { minPrice, maxPrice, location, bedrooms, take, direction, pages, lastId } =  getQueryData(query, yield select(getUserPropertiesData))
+    const { name, take, direction, pages, lastName, lastId } = getQueryData(query, yield select(getUserPropertiesData));
 
-    const response = yield fetch(`${BASE_URL}/properties/auth/findUserProperties/${take}/${lastName}/${lastId}/${direction}/${name}`)
+    console.log(`${BASE_URL}/properties/auth/findUserProperties/${take * pages}/${lastId}/${lastName}/${direction}/${name ? name.replace(/[\\?%#/'"]/g, '') : ''}`)
+    const response = yield fetch(`${BASE_URL}/properties/auth/findUserProperties/${take * pages}/${lastId}/${lastName}/${direction}/${name ? name : ''}`, {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('Authorization')}`
+        }
+    })
     const data = yield response.json();
-
     data.length = data.properties.length;
     data.properties = splitProperties(data, take)
 
-    yield put(setProperties({
+    yield put(setUserProperties({
         data,
         query
     }))
@@ -30,4 +33,18 @@ const splitProperties = (data, take) => {
     }, []);
 }
 
+const getQueryData = (query, state) => {
+    let lastId = 0; 
+    let lastName;
+    
+    if(!state.isInitial && !query.name){
+        const lastPage = state.properties[state.properties.length - 1];
+        const {id, name} = lastPage[lastPage.length - 1];
+
+        lastId = id;
+        lastName = name
+    }
+
+    return {...query, lastId, lastName}
+}
 
