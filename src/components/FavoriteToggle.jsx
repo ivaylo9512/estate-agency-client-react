a89f0e4df49c94cb794500c40fd762e97e8cce2b
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-06 18:33:06 +0300

    FavoriteToggle added

---
+++ src/components/FavoriteToggle.jsx
@@ -0,0 +1,39 @@
+import { memo, useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getPropertyFavoriteState, addFavorite, removeFavorite } from "../app/slices/toggleFavorite";
+import LoadingIndicator from "./LoadingIndicator";
+
+const FavoriteToggle = memo(({property}) => {
+    const [isFavorite, setIsFavorite] = useState(property.isFavorite);
+    const favoriteState = useSelector(getPropertyFavoriteState(property.id));
+    const dispatch = useDispatch();
+
+    const toggleFavorite = (property) => {
+        if(!isFavorite){
+            return dispatch(addFavorite(property.id));
+        }
+        dispatch(removeFavorite(property.id));
+    }
+
+    useEffect(() => {
+        console.log(favoriteState)
+        if(!favoriteState || favoriteState.isLoading){
+            return;
+        }
+
+        setIsFavorite(favoriteState.isFavorite)
+    },[favoriteState])
+
+    return(
+        <button onClick={() => toggleFavorite(property)}>{ 
+            favoriteState && favoriteState.isLoading 
+                ? <LoadingIndicator />
+                : isFavorite 
+                    ? 'remove'
+                    : 'add'
+        }
+        </button>
+    )
+
+})
+export default FavoriteToggle
