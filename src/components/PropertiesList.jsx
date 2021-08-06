a2a05f19da45f7ee68e4fddada9e89d79b6ba48c
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-06 17:43:55 +0300

    toggleFavorite added to PropertiesList + account page added

--- src/components/PropertiesList.jsx
+++ src/components/PropertiesList.jsx
@@ -1,10 +1,20 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { getPropertiesData, getProperties, getPropertiesState, setCurrentProperties } from "../app/slices/propertiesPaginationSlice"
 import LoadingIndicator from "../components/LoadingIndicator";
 import PropertiesPagination from "../components/PropertiesPagination";
+import { getToggleFavoriteState, addFavorite, removeFavorite } from "../app/slices/toggleFavorite";
 
 const PropertiesList = () => {
     const {currentProperties, isInitial, isLoading}  = useSelector(getPropertiesData)
+    const favorites = useSelector(getToggleFavoriteState);
+    const dispatch = useDispatch();
+
+    const toggleFavorite = (property) => {
+        if(!property.isFavorite){
+            return dispatch(addFavorite(property.id));
+        }
+        dispatch(removeFavorite(property.id));
+    }
 
     return(
         <div>
@@ -20,6 +30,15 @@ const PropertiesList = () => {
                                         <div key={property.id}>
                                             {property.id}
                                             {property.price}
+                                            <button onClick={() => toggleFavorite(property)}>{
+                                                favorites[property.id] 
+                                                    ? favorites[property.id].isLoading 
+                                                        ? <LoadingIndicator />
+                                                        : favorites[property.id].isFavorite 
+                                                            ? 'remove'
+                                                            : 'add'
+                                                    : property.isFavorite ? 'remove' : 'add'
+                                            }</button>
                                         </div>
                                     )
                                 }
---
+++ src/pages/account.jsx
@@ -0,0 +1,13 @@
+import UserPropertiesList from "../components/UserPropertiesList"
+import UserPropertiesForm from "../components/UserPropertiesForm"
+
+const Account = () => {
+
+    return(
+        <section>
+            <UserPropertiesForm />
+            <UserPropertiesList />
+        </section>
+    )
+}
+export default Account
