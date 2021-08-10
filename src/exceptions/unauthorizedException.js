32b5df06f5679cce666a4467b942089ab104f251
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-10 19:58:34 +0300

    UnauthorizedException added

---
+++ src/exceptions/unauthorizedException.js
@@ -0,0 +1,14 @@
+class UnauthorizedException extends Error{
+    constructor(message){
+        super(message)
+        Error.captureStackTrace(this, this.constructor);
+
+        this.name = this.constructor.name;
+        this.status = 401;
+    }
+
+    statusCode(){
+        return this.status;
+    }
+}
+export default UnauthorizedException
