00c76aec8245f5eaad317dae1285a2cbdb2f2ca5
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-14 23:49:18 +0300

    shanpshot login tests added

---
+++ src/pagesTests/login/snapshot.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Login from '../../pages/login';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import { getLoginRequest } from '../../app/slices/authenticate';
+
+jest.mock('react-redux', () => ({
+    useSelector: jest.fn(fn => fn()),
+    useDispatch: () => jest.fn(),
+}));
+
+jest.mock('../../app/slices/authenticate');
+
+const createWrapper = (state) => {
+    getLoginRequest.mockReturnValue(state);
+    return shallow(<Login /> )
+}
+describe("Login snapshot tests", () => {
+    it('renders correctly', () => {
+        const wrapper = createWrapper({isLoading: false, error: null});
+     
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('renders correctly with passed errors props and page 0', () => {
+        const wrapper = createWrapper({isLoading: false, error: 'Bad credentials.'});
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
