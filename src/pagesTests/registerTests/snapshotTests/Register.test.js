d8d82acfba239f67c7daa226d767b1a26599351b
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-14 17:39:31 +0300

    More Register snapshot tests

--- src/pagesTests/registerTests/snapshotTests/Register.test.js
+++ src/pagesTests/registerTests/snapshotTests/Register.test.js
@@ -17,7 +17,31 @@ const createWrapper = (value) => {
 }
 describe('RegisterSnapshotTests', () => {
     it('renders correctly page 0', () => {
-        const wrapper = createWrapper({ isLoading: false, error: null })
+        const wrapper = createWrapper({ isLoading: false, error: null });
         expect(toJson(wrapper)).toMatchSnapshot();
     });
+
+    it('renders correctly page 1', () => {
+        const wrapper = createWrapper({ isLoading: false, error: null });
+
+        const form = wrapper.find('form');
+        form.simulate('submite', { target: form, preventDefault: jest.fn() });
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    })
+
+    it('renders correctly with passed error props with page 0', () => {
+        const wrapper = createWrapper({isLoading: false, error: {username: 'Username is already taken.', password: 'Password must be between 10 and 20 characters.', email: 'Email is already taken.'}});
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    })
+
+    it('renders correctly with passed error props with page 1', () => {
+        const wrapper = createWrapper({isLoading: false, error: {country: 'Name is required.', location: 'location is required.', description: 'Description is required.'}});
+
+        const form = wrapper.find('form');
+        form.simulate('submit', { target: form, preventDefault: jest.fn() });
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    })
 })
