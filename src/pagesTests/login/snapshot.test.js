b5fecab13196601a29ad95c7737148d5ed291ed0
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-17 23:55:52 +0300

    to-json added to jest config + isomorphic-fetch

--- src/pagesTests/login/snapshot.test.js
+++ src/pagesTests/login/snapshot.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Login from '../../pages/login';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 import * as redux from 'react-redux';
 
 describe("Login snapshot tests", () => {
@@ -24,12 +23,12 @@ describe("Login snapshot tests", () => {
     it('renders correctly', () => {
         const wrapper = createWrapper({ isLoading: false, error: null });
      
-        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper).toMatchSnapshot();
     });
 
     it('renders correctly with passed errors props', () => {
         const wrapper = createWrapper({ isLoading: false, error: 'Bad credentials.' });
 
-        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper).toMatchSnapshot();
     });
 });
--- src/pagesTests/registerTests/snapshot.test.js
+++ src/pagesTests/registerTests/snapshot.test.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import Register from '../../pages/register';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 import * as redux from 'react-redux';
 
 describe('RegisterSnapshotTests', () => {
     let selectorSpy;
-    let dispatchSpy;
 
     beforeAll(() => {
         selectorSpy = jest.spyOn(redux, 'useSelector');
-        dispatchSpy = jest.spyOn(redux, 'useDispatch');
-
-        dispatchSpy.mockReturnValue(jest.fn());
+        jest.spyOn(redux, 'useDispatch').mockReturnValue(jest.fn());
     })
+
     const createWrapper = (state) => {
         selectorSpy.mockReturnValue(state);
         
@@ -24,7 +21,7 @@ describe('RegisterSnapshotTests', () => {
 
     it('renders correctly page 0', () => {
         const wrapper = createWrapper({ isLoading: false, error: null });
-        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper).toMatchSnapshot();
     });
 
     it('renders correctly page 1', () => {
@@ -33,13 +30,13 @@ describe('RegisterSnapshotTests', () => {
         const form = wrapper.find('form');
         form.simulate('submit', { target: form, preventDefault: jest.fn() });
 
-        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper).toMatchSnapshot();
     })
 
     it('renders correctly with passed error props with page 0', () => {
         const wrapper = createWrapper({ isLoading: false, error: { username: 'Username is already taken.', password: 'Password must be between 10 and 20 characters.', email: 'Email is already taken.' }});
 
-        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper).toMatchSnapshot();
     })
 
     it('renders correctly with passed error props with page 1', () => {
@@ -48,6 +45,6 @@ describe('RegisterSnapshotTests', () => {
         const form = wrapper.find('form');
         form.simulate('submit', { target: form, preventDefault: jest.fn() });
 
-        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper).toMatchSnapshot();
     })
 })
