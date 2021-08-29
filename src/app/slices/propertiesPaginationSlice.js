758b9c5926aaaa420907512e6a0833346af87cb6
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-29 07:43:15 +0300

    propertiesPagination + userPropertiesPagination slices data to dataInfo + isLoading/isInitial moved from dataInfo

--- src/app/slices/propertiesPaginationSlice.js
+++ src/app/slices/propertiesPaginationSlice.js
@@ -1,67 +1,71 @@
-const { createSlice } = require("@reduxjs/toolkit")
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    data:{
+    dataInfo: {
         pages: 0,
         maxPages: 0,
-        properties: [],
-        isLoading: false,
-        isInitial: true,
-        lastProperty: null,
-        currentProperties: undefined
+        data: [],
+        lastData: null,
+        currentData: null,
+        currentPage: 1,
     },
-    error: null,
     query: {
         take: 2,
         direction: 'ASC',
         bedrooms: 0,
         location: '',
         maxPrice: 0
-    }
+    },
+    isLoading: false,
+    isInitial: true,
+    error: null,
 }
 
 const propertiesPaginationSilce = createSlice({
     name: 'propertiesPagination',
     initialState,
     reducers: {
-        resetPropertiesState(state){
-            state.data = initialState.data
-            state.query = initialState.query;
-            state.error = initialState.error;
+        getProperties(state){
+            state.error = null
+            state.dataInfo.isLoading = true;
         },
         onPropertiesComplete(state, {payload: {pageable, query}}){
             state.query = query;
-            state.data.maxPages = state.data.pages + pageable.pages;
-            state.data.pages = state.data.pages + pageable.data.length;
-            state.data.lastProperty = pageable.lastProperty;
-            state.data.properties = [...state.data.properties, ...pageable.data];
-            state.data.currentProperties = pageable.data[pageable.data.length - 1]
-            state.data.isLoading = false;
-            state.data.isInitial = false;
+            state.dataInfo.maxPages = state.dataInfo.pages + pageable.pages;
+            state.dataInfo.pages = state.dataInfo.pages + pageable.data.length;
+            state.dataInfo.currentPage = state.dataInfo.pages;
+            state.dataInfo.lastData = pageable.lastProperty;
+            state.dataInfo.data = [...state.dataInfo.data, ...pageable.data];
+            state.dataInfo.currentData = pageable.data[pageable.data.length - 1] || [];
+            state.isLoading = false;
+            state.isInitial = false;
             state.error = null
         },
         onPropertiesFail(state, {payload}){
-            state.data.isLoading = false
+            state.dataInfo.isLoading = false
             state.error = payload
-        },
-        getProperties(state){
-            state.error = null
-            state.data.isLoading = true;
         },
         setPropertiesDirection(state, action){
             state.query = initialState.query
-            state.data = initialState.data
+            state.dataInfo = initialState.dataInfo
             state.error = initialState.error;
             state.query.direction = action.payload.direction
         },
         setCurrentProperties(state, action){
-            state.data.currentProperties = action.payload
-        }
+            state.dataInfo.currentProperties = action.payload
+        },
+        resetPropertiesState(state){
+            state.isLoading = initialState.isLoading;
+            state.isInitial = initialState.isInitial;
+            state.dataInfo = initialState.data
+            state.query = initialState.query;
+            state.error = initialState.error;
+        },
     }
 })
-export const {resetPropertiesState, onPropertiesComplete, onPropertiesFail, getProperties, setPropertiesDirection, setCurrentProperties} = propertiesPaginationSilce.actions
+export const { resetPropertiesState, onPropertiesComplete, onPropertiesFail, getProperties, setPropertiesDirection, setCurrentProperties } = propertiesPaginationSilce.actions
 export default propertiesPaginationSilce.reducer
 
-export const getPropertiesData = state => state.propertiesPagination.data;
+export const getPropertiesData = state => state.propertiesPagination.dataInfo;
 export const getPropertiesQuery = state => state.propertiesPagination.query;
 export const getPropertiesState = state => state.propertiesPagination;
--- src/app/slices/userPropertiesPaginationSlice.js
+++ src/app/slices/userPropertiesPaginationSlice.js
@@ -1,50 +1,48 @@
-const { createSlice } = require("@reduxjs/toolkit")
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    data:{
+    dataInfo:{
         pages: 0,
         maxPages: 0,
-        properties: [],
-        lastProperty: null,
-        isLoading: false,
-        isInitial: true,
-        currentProperties: null
+        data: [],
+        lastData: null,
+        currentData: null,
+        currentPage: 1,
+
     },
-    error: null,
     query: {
         take: 2,
         direction: 'ASC',
         name: '',
-    }
+    },
+    isLoading: false,
+    isInitial: true,
+    error: null,
 }
 
 const userPropertiesPaginationSilce = createSlice({
     name: 'userPropertiesPagination',
     initialState,
     reducers: {
-        resetUserPropertiesState(state){
-            state.error = null
-            state.data = initialState.data
-            state.query = initialState.query;
+        getUserProperties(state){
+            state.isLoading = true;
+            state.error = null;
         },
         onUserPropertiesComplete(state, {payload: {pageable, query}}){
-            state.error = null
             state.query = query;
-            state.data.maxPages = state.data.pages + pageable.pages;
-            state.data.pages = state.data.pages + pageable.data.length;
-            state.data.lastProperty = pageable.lastProperty;
-            state.data.properties = [...state.data.properties, ...pageable.data];
-            state.data.currentProperties = pageable.data[pageable.data.length - 1]
-            state.data.isLoading = false;
-            state.data.isInitial = false;
+            state.dataInfo.maxPages = state.dataInfo.pages + pageable.pages;
+            state.dataInfo.pages = state.dataInfo.pages + pageable.data.length;
+            state.dataInfo.currentPage = state.dataInfo.pages;
+            state.dataInfo.lastData = pageable.lastProperty;
+            state.dataInfo.data = [...state.dataInfo.data, ...pageable.data];
+            state.dataInfo.currentData = pageable.data[pageable.data.length - 1] || [];
+            state.isLoading = false;
+            state.isInitial = false;
+            state.error = null
         },
-        onUserPropertiesFail(state, {payload}){
-            state.data.isLoading = false
+        onUserPropertiesError(state, {payload}){
+            state.isLoading = false
             state.error = payload
-        },
-        getUserProperties(state){
-            state.data.isLoading = true;
-            state.error = null;
         },
         setUserPropertiesDirection(state, action){
             state.error = null
@@ -54,10 +52,17 @@ const userPropertiesPaginationSilce = createSlice({
         },
         setCurrentUserProperties(state, action){
             state.data.currentProperties = action.payload
-        }
+        },
+        resetUserPropertiesState(state){
+            state.error = initialState.error;
+            state.isLoading = initialState.isLoading;
+            state.isInitial = initialState.isInitial;
+            state.data = initialState.data;
+            state.query = initialState.query;
+        },
     }
 })
-export const {resetUserPropertiesState, onUserPropertiesComplete,onUserPropertiesFail, getUserProperties, setUserPropertiesDirection, setCurrentUserProperties} = userPropertiesPaginationSilce.actions
+export const {resetUserPropertiesState, onUserPropertiesComplete, onUserPropertiesError, getUserProperties, setUserPropertiesDirection, setCurrentUserProperties} = userPropertiesPaginationSilce.actions
 export default userPropertiesPaginationSilce.reducer
 
 export const getUserPropertiesData = state => state.userPropertiesPagination.data;
