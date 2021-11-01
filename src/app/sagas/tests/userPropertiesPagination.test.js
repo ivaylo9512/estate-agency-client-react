90f4fc9a5a71c5b2278373598ddc840c32905e4a
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-01 23:37:44 +0200

    more userPropertiesPagination tests

--- src/app/sagas/tests/userPropertiesPagination.test.js
+++ src/app/sagas/tests/userPropertiesPagination.test.js
@@ -3,10 +3,12 @@ import { call, select } from 'redux-saga/effects';
 import { BASE_URL } from 'appConstants';
 import propertiesReducer, { onUserPropertiesComplete, onUserPropertiesError, getUserPropertiesData } from 'app/slices/userPropertiesPaginationSlice';
 import { getProperties } from 'app/sagas/userPropertiesPagination';
+import { wrapper } from 'app/sagas'
 import 'isomorphic-fetch'
+import { onLogout } from 'app/slices/authenticateSlice';
 
 describe('user properties saga tests', () => {
-    it('should set state wiht split array with query take on user properties request', () => {
+    it('should set state with split array with query take on user properties request', () => {
         const lastData = {
             id: 3, 
             name: 'testC'
@@ -85,6 +87,139 @@ describe('user properties saga tests', () => {
             .run();
     })
 
+    it('should set state with initial state', () => {
+        const query = {
+            pages: 2,
+            take: 3,
+            direction: 'ASC',
+            name: 'test',
+        }
+
+        const pageable = {
+            data: [{ id: 4, name: 'testD' }, { id: 7, name: 'testE' }, { id: 9, name: 'testF' }, 
+                { id: 12, name: 'testG' }, { id: 13, name: 'testH' }, { id: 14, name: 'testJ' }],
+            count: 12
+        }
+
+        const lastData = pageable.data[5];
+        const completePayload = {
+            pageable: {
+                data: [[ pageable.data[0], pageable.data[1], pageable.data[2] ], 
+                    [ pageable.data[3], pageable.data[4], pageable.data[5] ]],
+                lastProperty: lastData,
+                pages: 4,
+                count: 12
+            },
+            query
+        }
+
+        return expectSaga(getProperties, { payload: query })
+            .withReducer(propertiesReducer)
+            .provide([
+                [call(fetch, `${BASE_URL}/properties/auth/findUserProperties/${query.take * query.pages}/0/undefined/${query.direction}/${query.name}`,{
+                    headers:{
+                        Authorization: null
+                    }
+                }), new Response(JSON.stringify(pageable), { status: 200 })],
+                [select(getUserPropertiesData), { dataInfo: { lastData: null }}]
+            ])
+            .put(onUserPropertiesComplete(completePayload))
+            .hasFinalState({
+                dataInfo: {
+                    pages: 2,
+                    maxPages: 4,
+                    currentPage: 2,
+                    data: completePayload.pageable.data,
+                    currentData: completePayload.pageable.data[1],
+                    lastData,
+                },
+                error: null,
+                isLoading: false,
+                isInitial: false,
+                query
+            })
+            .run();
+    })
+
+    it('should set state wihtout query name', () => {
+        const lastData = {
+            id: 3, 
+            name: 'testC'
+        }
+
+        const currentData = [ { id: 2, name: 'testA' }, { id: 5, name: 'testB' }, lastData ];
+
+        const state = {
+            dataInfo: {
+                pages: 1,
+                maxPages: 2,
+                data: [ currentData ],
+                lastData,
+                currentData,
+                currentPage: 1
+            },
+            query: {
+                take: 3,
+                direction: 'ASC',
+            },
+            isLoading: true,
+            error: null
+        }
+
+        const query = {
+            pages: 2,
+            take: 3,
+            direction: 'ASC',
+            name: '',
+        }
+
+        const pageable = {
+            data: [{ id: 4, name: 'testD' }, { id: 7, name: 'testE' }, { id: 9, name: 'testF' }, 
+                { id: 12, name: 'testG' }, { id: 13, name: 'testH' }, { id: 14, name: 'testJ' }],
+            count: 12
+        }
+
+        const newLastData = pageable.data[5];
+        const completePayload = {
+            pageable: {
+                data: [[ pageable.data[0], pageable.data[1], pageable.data[2] ], 
+                    [ pageable.data[3], pageable.data[4], pageable.data[5] ]],
+                lastProperty: newLastData,
+                pages: 4,
+                count: 12
+            },
+            query
+        }
+
+        return expectSaga(getProperties, { payload: query })
+            .withReducer(propertiesReducer)
+            .withState(state)
+            .provide([
+                [call(fetch, `${BASE_URL}/properties/auth/findUserProperties/${query.take * query.pages}/${lastData.id}/testC/${query.direction}/`,{
+                    headers:{
+                        Authorization: null
+                    }
+                }), new Response(JSON.stringify(pageable), { status: 200 })],
+                [select(getUserPropertiesData), state.dataInfo]
+            ])
+            .put(onUserPropertiesComplete(completePayload))
+            .hasFinalState({
+                dataInfo: {
+                    pages: 3,
+                    maxPages: 5,
+                    currentPage: 3,
+                    data: [...state.dataInfo.data, ...completePayload.pageable.data],
+                    currentData: completePayload.pageable.data[1],
+                    lastData: newLastData,
+                },
+                error: null,
+                isLoading: false,
+                isInitial: false,
+                query
+            })
+            .run();
+    })
+
     it('should set error on user chats request error', () => {
         const error = '0 is not a valid take value';
 
@@ -127,4 +262,155 @@ describe('user properties saga tests', () => {
             .run()
     })
 
+    it('should call onLogout on users properties error with 401', () => {
+        const query = {
+            take: 2,
+            direction: 'ASC',
+            pages: 1
+        }
+
+        return expectSaga(wrapper(getProperties), { payload: query })
+            .withReducer(propertiesReducer)
+            .provide([
+                [call(fetch, `${BASE_URL}/properties/auth/findUserProperties/2/0/undefined/${query.direction}/`, {
+                    headers: {
+                        Authorization: null
+                    }
+                }), new Response('jwt expired', { status: 401 })],
+                [select(getUserPropertiesData), { data: { lastData: null } }]
+            ])
+            .put(onLogout('Session has expired.'))
+            .run()
+    })
+
+    it('should set state with reponse with an empty data array', () => {
+        const lastData = {
+            id: 3, 
+            name: 'testC'
+        }
+
+        const currentData = [ { id: 2, name: 'testA' }, { id: 5, name: 'testB' }, lastData ];
+
+        const state = {
+            dataInfo: {
+                pages: 1,
+                maxPages: 2,
+                data: [ currentData ],
+                lastData,
+                currentData,
+                currentPage: 1
+            },
+            query: {
+                take: 3,
+                direction: 'ASC',
+            },
+            isLoading: true,
+            error: null
+        }
+
+        const query = {
+            pages: 2,
+            take: 3,
+            direction: 'ASC',
+            name: 'test',
+        }
+
+        const pageable = {
+            data: [],
+            count: 0
+        }
+
+        const completePayload = {
+            pageable: {
+                data: [],
+                lastProperty: lastData,
+                pages: 0,
+                count: 0
+            },
+            query
+        }
+
+        return expectSaga(getProperties, { payload: query })
+            .withReducer(propertiesReducer)
+            .withState(state)
+            .provide([
+                [call(fetch, `${BASE_URL}/properties/auth/findUserProperties/${query.take * query.pages}/${lastData.id}/undefined/${query.direction}/${query.name}`,{
+                    headers:{
+                        Authorization: null
+                    }
+                }), new Response(JSON.stringify(pageable), { status: 200 })],
+                [select(getUserPropertiesData), state.dataInfo]
+            ])
+            .put(onUserPropertiesComplete(completePayload))
+            .hasFinalState({
+                dataInfo: {
+                    pages: 1,
+                    maxPages: 1,
+                    currentPage: 1,
+                    data: [ currentData ],
+                    currentData,
+                    lastData,
+                },
+                error: null,
+                isLoading: false,
+                isInitial: false,
+                query
+            })
+            .run();
+    })
+
+    it('should set state with auth token', () => {
+        localStorage.setItem('Authorization', 'token')
+        const query = {
+            pages: 2,
+            take: 3,
+            direction: 'ASC',
+            name: 'test',
+        }
+
+        const pageable = {
+            data: [{ id: 4, name: 'testD' }, { id: 7, name: 'testE' }, { id: 9, name: 'testF' }, 
+                { id: 12, name: 'testG' }, { id: 13, name: 'testH' }, { id: 14, name: 'testJ' }],
+            count: 12
+        }
+
+        const lastData = pageable.data[5];
+        const completePayload = {
+            pageable: {
+                data: [[ pageable.data[0], pageable.data[1], pageable.data[2] ], 
+                    [ pageable.data[3], pageable.data[4], pageable.data[5] ]],
+                lastProperty: lastData,
+                pages: 4,
+                count: 12
+            },
+            query
+        }
+
+        return expectSaga(getProperties, { payload: query })
+            .withReducer(propertiesReducer)
+            .provide([
+                [call(fetch, `${BASE_URL}/properties/auth/findUserProperties/${query.take * query.pages}/0/undefined/${query.direction}/${query.name}`,{
+                    headers:{
+                        Authorization: 'Bearer token'
+                    }
+                }), new Response(JSON.stringify(pageable), { status: 200 })],
+                [select(getUserPropertiesData), { dataInfo: { lastData: null }}]
+            ])
+            .put(onUserPropertiesComplete(completePayload))
+            .hasFinalState({
+                dataInfo: {
+                    pages: 2,
+                    maxPages: 4,
+                    currentPage: 2,
+                    data: completePayload.pageable.data,
+                    currentData: completePayload.pageable.data[1],
+                    lastData,
+                },
+                error: null,
+                isLoading: false,
+                isInitial: false,
+                query
+            })
+            .run();
+    })
 })
--- src/app/sagas/userPropertiesPagination.js
+++ src/app/sagas/userPropertiesPagination.js
@@ -2,11 +2,13 @@ import { BASE_URL } from "appConstants";
 import { getUserPropertiesData, onUserPropertiesError, onUserPropertiesComplete } from "app/slices/userPropertiesPaginationSlice";
 import { takeLatest, select, put, call } from 'redux-saga/effects';
 import splitArray from "utils/splitArray";
+import { wrapper } from ".";
+import UnauthorizedException from "exceptions/unauthorizedException";
 
-export default takeLatest('userPropertiesPagination/getUserProperties', getProperties)
+export default takeLatest('userPropertiesPagination/getUserProperties', wrapper(getProperties))
 
 export function* getProperties({payload: query}) {
-    const { name, take, direction, pages, lastName, lastId } = getQueryData(query, yield select(getUserPropertiesData));
+    const { name, take, direction, pages, lastName, lastId, lastData } = getQueryData(query, yield select(getUserPropertiesData));
 
     const response = yield call(fetch, `${BASE_URL}/properties/auth/findUserProperties/${take * pages}/${lastId}/${lastName}/${direction}/${name ? name : ''}`, {
         headers: {
@@ -17,7 +19,7 @@ export function* getProperties({payload: query}) {
         const pageable = yield response.json();
 
         pageable.pages = Math.ceil(pageable.count / query.take);
-        pageable.lastProperty = pageable.data[pageable.data.length - 1];
+        pageable.lastProperty = pageable.count == 0 ? lastData : pageable.data[pageable.data.length - 1];
         pageable.data = splitArray(pageable.data, query.take);
     
         yield put(onUserPropertiesComplete({
@@ -25,27 +27,29 @@ export function* getProperties({payload: query}) {
             query
         }))        
     }else{
-        yield put(onUserPropertiesError(yield response.text()));
-
+        const error = yield response.text();
         if(response.status == 401){
-            throw new UnauthorizedException(message);            
+            throw new UnauthorizedException(error);            
         } 
+
+        yield put(onUserPropertiesError(error));
     }
 }
 
-const getQueryData = (query, state) => {
+const getQueryData = (query, data) => {
+    const lastData = data.lastData;
     let lastId = 0; 
     let lastName;
     let takeAmount = query.take * query.pages;
-    
-    if(state.lastData){
-        const {id, name} = state.lastData;
+
+    if(lastData){
+        const {id, name} = lastData;
 
         lastId = id;
 
         if(!query.name) lastName = name
     }
 
-    return {...query, lastId, lastName, takeAmount}
+    return { ...query, lastId, lastName, takeAmount, lastData }
 }
 
